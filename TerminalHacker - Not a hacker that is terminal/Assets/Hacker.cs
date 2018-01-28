51aa6dc43accef6dee7bf27f7edf4e42a92e9125
Feureau-PC <feureau@gmail.com>
2018-01-29 02:15:04 +0700

    015 Functions With Variable Parameters

--- TerminalHacker - Not a hacker that is terminal/Assets/Hacker.cs
+++ TerminalHacker - Not a hacker that is terminal/Assets/Hacker.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class Hacker : MonoBehaviour {
     // Use this for initialization
     void Start() {
-        ShowMainMenu();
+        ShowMainMenu("Hello Ben");
+        CookCurry("lamb");
     }
 
-    void ShowMainMenu() {
+    void ShowMainMenu(string greeting) {
         Terminal.ClearScreen();
+        Terminal.WriteLine(greeting);
         Terminal.WriteLine("What would you like to hack into?");
         Terminal.WriteLine("Press 1 for the local library");
         Terminal.WriteLine("Press 2 for the police station");
         Terminal.WriteLine("Enter your selection");
+    }
+
+    void CookCurry(string meatToUse) {
+        // common cooking steps go here
+        print("I am adding the " + meatToUse);
     }
 
     // Update is called once per frame
