7d971733263c25d4228d6ac920fc13dad8a1662c
Feureau-PC <feureau@gmail.com>
2018-01-29 00:09:15 +0700

    012 Using Our Terminal.WriteLine

---
+++ TerminalHacker - Not a hacker that is terminal/Assets/Hacker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Hello Console");
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Enter your selection");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
