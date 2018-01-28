9409353f4016afb57422d59db9d7e69bfa0830d8
Feureau-PC <feureau@gmail.com>
2018-01-29 03:22:46 +0700

    018 Conditional Program Flow Using if

--- TerminalHacker - Not a hacker that is terminal/Assets/Hacker.cs
+++ TerminalHacker - Not a hacker that is terminal/Assets/Hacker.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class Hacker : MonoBehaviour {
     // Use this for initialization
     void Start() {
-        ShowMainMenu("Hello Ben");
         CookCurry("lamb");
     }
 
-    void ShowMainMenu(string greeting) {
+    void ShowMainMenu() {
         Terminal.ClearScreen();
-        Terminal.WriteLine(greeting);
         Terminal.WriteLine("What would you like to hack into?");
         Terminal.WriteLine("Press 1 for the local library");
         Terminal.WriteLine("Press 2 for the police station");
@@ -22,8 +20,15 @@ public class Hacker : MonoBehaviour {
         // common cooking steps go here
         print("I am adding the " + meatToUse);
     }
-
-    // Update is called once per frame
-    void Update() {
+    void OnUserInput(string input) {
+        if (input == "menu") {
+            ShowMainMenu();
+        }
+        else if (input = "007") {
+            Terminal.WriteLine("Please select a level, Mr. Bond");
+        }
+        else {
+            Terminal.WriteLine("Please choose a valid level");
+        }
     }
 }
