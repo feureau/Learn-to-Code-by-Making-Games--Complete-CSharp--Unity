cd1eb4bdc61c2c2db3b5058cc416309a3077ea6b
Feureau-PC <feureau@gmail.com>
2018-01-29 16:12:15 +0700

    022 Enumerating Our Game States

--- TerminalHacker - Not a hacker that is terminal/Assets/Hacker.cs
+++ TerminalHacker - Not a hacker that is terminal/Assets/Hacker.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour {
+    int level; // game state
+    Screen currentScreen = Screen.MainMenu;
+    enum Screen {
+        MainMenu,
+        Password,
+        Win
+    };
     // Use this for initialization
     void Start() {
         CookCurry("lamb");
@@ -20,15 +27,30 @@ public class Hacker : MonoBehaviour {
         // common cooking steps go here
         print("I am adding the " + meatToUse);
     }
+
     void OnUserInput(string input) {
         if (input == "menu") {
             ShowMainMenu();
         }
-        else if (input = "007") {
+        else if (input == "1") {
+            level = 1;
+            StartGame();
+        }
+        else if (input == "2") {
+            level = 2;
+            StartGame();
+        }
+        else if (input == "007") {
             Terminal.WriteLine("Please select a level, Mr. Bond");
         }
         else {
             Terminal.WriteLine("Please choose a valid level");
         }
+    }
+
+    void StartGame() {
+        currentScreen = Screen.Password;
+        Terminal.WriteLine("You have chosen level " + level);
+        Terminal.WriteLine("Please enter your password: ");
     }
 }
